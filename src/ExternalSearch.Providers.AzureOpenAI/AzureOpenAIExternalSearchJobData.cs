c3c5e9b40131b1880d2c218fa9fcc6bbc464f375
andrew-hardy <aha@cluedin.com>
2025-11-04 11:36:31 +1000

    feat: support CluedIn 4.7.0 (#17)

--- src/ExternalSearch.Providers.AzureOpenAI/AzureOpenAIExternalSearchJobData.cs
+++ src/ExternalSearch.Providers.AzureOpenAI/AzureOpenAIExternalSearchJobData.cs
@@ -8,10 +8,14 @@ public class AzureOpenAIExternalSearchJobData : CrawlJobData
     public AzureOpenAIExternalSearchJobData(IDictionary<string, object> configuration)
     {
         AcceptedEntityType = GetValue<string>(configuration, Constants.KeyName.AcceptedEntityType);
+        BaseUrl = GetValue<string>(configuration, Constants.KeyName.BaseUrl);
+        ApiKey = GetValue<string>(configuration, Constants.KeyName.ApiKey);
         AiDeployment = GetValue<string>(configuration, Constants.KeyName.AiDeployment);
         Prompt = GetValue<string>(configuration, Constants.KeyName.Prompt);
     }
 
+    public string BaseUrl { get; set; }
+    public string ApiKey { get; set; }
     public string AcceptedEntityType { get; set; }
     public string AiDeployment { get; set; }
     public string Prompt { get; set; }
@@ -21,6 +25,8 @@ public class AzureOpenAIExternalSearchJobData : CrawlJobData
         return new Dictionary<string, object>
         {
             { Constants.KeyName.AcceptedEntityType, AcceptedEntityType },
+            { Constants.KeyName.BaseUrl, BaseUrl },
+            { Constants.KeyName.ApiKey, ApiKey },
             { Constants.KeyName.AiDeployment, AiDeployment },
             { Constants.KeyName.Prompt, Prompt },
         };
